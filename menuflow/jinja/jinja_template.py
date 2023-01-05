d6b553ad19c7042745e85045b375b5c950f1667c
Alejandro Herrera <bramendev@gmail.com>
2023-01-04 21:36:29 -0500

    Added better jinja filters and doc for these

--- menuflow/jinja/jinja_template.py
+++ menuflow/jinja/jinja_template.py
@@ -1,7 +1,35 @@
 from datetime import datetime
+from re import match
 
-from jinja2 import Environment
+from jinja2 import BaseLoader, Environment
 
-jinja_env = Environment(autoescape=True)
+jinja_env = Environment(autoescape=True, loader=BaseLoader)
 
-jinja_env.globals.update(date_isoformat=lambda: datetime.utcnow().isoformat())
+jinja_env.globals.update(utcnow_isoformat=lambda: datetime.utcnow().isoformat())
+"""
+Return the time formatted according to ISO.
+e.g
+{{ utcnow_isoformat() }}
+"""
+
+jinja_env.globals.update(utcnow=lambda: datetime.utcnow())
+"""
+Construct a UTC datetime from time.time().
+e.g
+{{ utcnow() }}
+"""
+
+jinja_env.globals.update(datetime_format=lambda date, format: datetime.strptime(date, format))
+"""
+Converts a string to a datetime with a specific format
+e.g
+{{ datetime_format(14 09 1999, "%d %m %Y") }}
+"""
+
+
+jinja_env.globals.update(match=lambda pattern, value: bool(match(pattern, value)))
+"""
+Validates if a pattern matches a variable
+e.g
+{{ match("^(0[1-9]|[12][0-9]|3[01])\s(0[1-9]|1[012])\s(19[0-9][0-9]|20[0-9][0-9])$", "14 09 1999") }}
+"""
