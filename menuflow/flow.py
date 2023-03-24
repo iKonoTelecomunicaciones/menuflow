6e889e489c461b94d74f1019d8b4c8e61e9bc211
Alejandro Herrera <bramendev@gmail.com>
2023-03-24 09:45:33 -0500

    Added github action job for tests

--- menuflow/flow.py
+++ menuflow/flow.py
@@ -6,10 +6,12 @@ from typing import TYPE_CHECKING, Dict
 from mautrix.types import SerializableAttrs
 from mautrix.util.logging import TraceLogger
 
-from .middlewares import HTTPMiddleware
 from .nodes import CheckTime, Email, HTTPRequest, Input, Media, Message, Switch
 from .repository import Flow as FlowModel
 from .room import Room
+
+if TYPE_CHECKING:
+    from .middlewares import HTTPMiddleware
 
 
 class Flow:
