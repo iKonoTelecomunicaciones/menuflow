38f53c158520c18cc365036fcd716e34ccf0cf82
egalvis <egalvis@ikono.com.co>
2023-11-16 10:13:38 -0500

    feat(events): :sparkles: Parametrized event log generation

--- menuflow/events/base_event.py
+++ menuflow/events/base_event.py
@@ -9,6 +9,7 @@ from mautrix.types import SerializableAttrs, UserID
 from mautrix.util.logging import TraceLogger
 from nats.js.client import JetStreamContext
 
+from ..config import Config
 from .event_types import MenuflowEventTypes, MenuflowNodeEvents
 from .nats_publisher import NatsPublisher
 
@@ -22,17 +23,20 @@ class BaseEvent(SerializableAttrs):
     timestamp: float = ib(factory=float)
     sender: UserID = ib(factory=UserID)
 
-    def send(self):
+    def send(self, config: Config):
+        log.error(f"Sending event {self.serialize()}")
         asyncio.create_task(self.send_to_nats())
 
-    async def send_to_nats(self):
-        jetstream: JetStreamContext = None
+        if config["events.write_to_file"]:
+            self.write_to_file()
 
+    def write_to_file(self):
         file = open("/data/room_events.txt", "a")
         file.write(f"{json.dumps(self.serialize())}\n\n")
         file.close()
-        log.error(f"Sending event {self.serialize()}")
 
+    async def send_to_nats(self):
+        jetstream: JetStreamContext = None
         _, jetstream = await NatsPublisher.get_connection()
         if jetstream:
             try:
