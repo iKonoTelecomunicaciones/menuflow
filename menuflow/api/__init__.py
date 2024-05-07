e0d1f3880033a6671fac93f21e12be1f0a4c2666
Esteban Galvis <egalvis@ikono.com.co>
2024-05-07 16:52:53 -0500

    feat(api): :sparkles: Added cors config to api

--- menuflow/api/__init__.py
+++ menuflow/api/__init__.py
@@ -1,5 +1,6 @@
 from asyncio import AbstractEventLoop
 
+import aiohttp_cors
 from aiohttp import web
 
 import menuflow
@@ -14,4 +15,19 @@ def init(cfg: Config, loop: AbstractEventLoop) -> web.Application:
     set_config(cfg)
     app = web.Application(loop=loop, client_max_size=100 * 1024 * 1024)
     app.add_routes(routes)
+    cors = aiohttp_cors.setup(
+        app,
+        defaults={
+            "*": aiohttp_cors.ResourceOptions(
+                allow_credentials=True,
+                expose_headers="*",
+                allow_headers="*",
+                allow_methods=["GET", "POST", "OPTIONS", "PATCH"],
+            )
+        },
+    )
+
+    for route in list(app.router.routes()):
+        cors.add(route)
+
     return app
