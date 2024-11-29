23bc8aa367043ff0047c2c8be5ae48470fe8bc65
Esteban Galvis <egalvis@ikono.com.co>
2024-11-29 10:25:46 -0500

    build: :building_construction: Improved project image build

--- run.sh
+++ run.sh
@@ -12,15 +12,11 @@ if [ ! -f /data/config.yaml ]; then
 fi
 
 if [ "$1" = "dev" ]; then
-	pip install --ignore-installed -r requirements-dev.txt
 	# Configure git to use the safe directory
 	if ! [ $(git config --global --get safe.directory) ]; then
 		echo "Setting safe.directory config to /opt/menuflow"
 		git config --global --add safe.directory /opt/menuflow
 	fi
-	# Getting the version from git repository
-	python3 setup.py --version
-    watchmedo auto-restart -R -p="*.py" -d="." /opt/menuflow/run.sh
 fi
 
 exec python3 -m menuflow -c /data/config.yaml
