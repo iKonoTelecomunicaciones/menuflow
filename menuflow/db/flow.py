f4e714d57c51b6f3d6a26e719ea11e2b5f27ef0d
Esteban Galvis Triana <86634281+egalvis39@users.noreply.github.com>
2025-02-06 13:53:38 -0500

    feat: :sparkles: Save flow backups (#211)
    * feat: :sparkles: Save flow backups
    
    * feat: :sparkles: Improved get backup endpoint
    
    * docs: :memo: Improved documentation
    
    * feat: :sparkles: Fix error in function name

--- menuflow/db/flow.py
+++ menuflow/db/flow.py
@@ -6,6 +6,9 @@ from attr import dataclass, ib
 from mautrix.types import SerializableAttrs
 from mautrix.util.async_db import Database
 
+from ..config import Config
+from .flow_backup import FlowBackup
+
 fake_db = Database.create("") if TYPE_CHECKING else None
 
 
@@ -16,22 +19,13 @@ class Flow(SerializableAttrs):
     id: int = ib(default=None)
     flow: Dict[str, Any] = ib(factory=dict)
 
-    @classmethod
-    def _from_row(cls, row: Record) -> Union["Flow", None]:
-        return cls(id=row["id"], flow=json.loads(row["flow"]))
-
     @property
     def values(self) -> Dict[str, Any]:
         return json.dumps(self.flow)
 
-    async def insert(self) -> int:
-        q = "INSERT INTO flow (flow) VALUES ($1)"
-        await self.db.execute(q, self.values)
-        return await self.db.fetchval("SELECT MAX(id) FROM flow")
-
-    async def update(self) -> None:
-        q = "UPDATE flow SET flow=$1 WHERE id=$2"
-        await self.db.execute(q, self.values, self.id)
+    @classmethod
+    def _from_row(cls, row: Record) -> Union["Flow", None]:
+        return cls(id=row["id"], flow=json.loads(row["flow"]))
 
     @classmethod
     async def all(cls) -> list[Dict]:
@@ -61,3 +55,19 @@ class Flow(SerializableAttrs):
             return
 
         return cls._from_row(row)
+
+    async def insert(self) -> int:
+        q = "INSERT INTO flow (flow) VALUES ($1)"
+        await self.db.execute(q, self.values)
+        return await self.db.fetchval("SELECT MAX(id) FROM flow")
+
+    async def update(self) -> None:
+        q = "UPDATE flow SET flow=$1 WHERE id=$2"
+        await self.db.execute(q, self.values, self.id)
+
+    async def backup_flow(self, config: Config) -> None:
+        backup_count = await FlowBackup.get_count_by_flow_id(self.id)
+        if backup_count >= config["menuflow.backup_limit"]:
+            await FlowBackup.delete_orldest_by_flow_id(self.id)
+
+        await FlowBackup(flow_id=self.id, flow=self.flow).insert()
--- menuflow/web/api/flow.py
+++ menuflow/web/api/flow.py
@@ -8,6 +8,7 @@ from aiohttp import web
 from ...config import Config
 from ...db.client import Client as DBClient
 from ...db.flow import Flow as DBFlow
+from ...db.flow_backup import FlowBackup
 from ...menu import MenuClient
 from ..base import get_config, routes
 from ..responses import resp
@@ -69,6 +70,9 @@ async def create_or_update_flow(request: web.Request) -> web.Response:
 
     if flow_id:
         flow = await DBFlow.get_by_id(flow_id)
+        if flow.flow != incoming_flow:
+            await flow.backup_flow(config)
+
         flow.flow = incoming_flow
         await flow.update()
 
@@ -132,3 +136,64 @@ async def get_flow(request: web.Request) -> web.Response:
         data = {"flows": flows}
 
     return resp.ok(data)
+
+
+@routes.get("/v1/flow/{flow_id}/backup", allow_head=False)
+async def get_backup(request: web.Request) -> web.Response:
+    """
+    ---
+    summary: Get flow backups by flow ID.
+    tags:
+        - Flow
+
+    parameters:
+        - in: path
+          name: flow_id
+          description: The flow ID to get the backups.
+          required: true
+          schema:
+            type: integer
+
+        - in: query
+          name: limit
+          description: The limit of backups to get.
+          schema:
+            type: integer
+
+        - in: query
+          name: offset
+          description: The offset of backups to get.
+          schema:
+            type: integer
+
+        - in: query
+          name: backup_id
+          description: The backup ID to get.
+          schema:
+              type: integer
+
+    responses:
+        '200':
+            $ref: '#/components/responses/GetFlowBackupsSuccess'
+        '404':
+            $ref: '#/components/responses/GetFlowBackupsNotFound'
+
+    """
+    offset = int(request.query.get("offset", 0))
+    limit = int(request.query.get("limit", 10))
+    backup_id = request.query.get("backup_id", None)
+
+    flow_id = int(request.match_info["flow_id"])
+    flow = await DBFlow.get_by_id(int(flow_id))
+    if not flow:
+        return resp.not_found(f"Flow with ID {flow_id} not found")
+
+    if backup_id:
+        backup = await FlowBackup.get_by_id(int(backup_id))
+        if not backup:
+            return resp.not_found(f"Backup with ID {backup_id} not found")
+        return resp.ok(backup.to_dict())
+
+    count = await FlowBackup.get_count_by_flow_id(flow_id=flow_id)
+    backups = await FlowBackup.all_by_flow_id(flow_id=flow_id, offset=offset, limit=limit)
+    return resp.ok({"count": count, "backups": [backup.to_dict() for backup in backups]})
