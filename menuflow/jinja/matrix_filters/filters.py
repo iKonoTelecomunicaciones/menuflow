24cc0f850dd0ec2bc7abd1862149212248433208
@jcardenas4773819 <jcardenas@ikono.com.co>
2025-03-28 13:57:06 -0500

    refactor: :recycle: return a tuple instead string in matrix jinja filters

--- menuflow/jinja/matrix_filters/filters.py
+++ menuflow/jinja/matrix_filters/filters.py
@@ -5,18 +5,18 @@ def user_bridge_info(user_id: str) -> str:
     user_bridge_match = match("^@(?P<user_prefix>.+)_(?P<account_id>[0-9]{8,}):.+$", user_id)
 
     if not user_bridge_match:
-        return ","
+        return "", ""
 
-    return f"{user_bridge_match.group('user_prefix')},{user_bridge_match.group('account_id')}"
+    return user_bridge_match.group("user_prefix"), user_bridge_match.group("account_id")
 
 
 def user_bridge_prefix(user_id: str) -> str:
-    prefix, _ = user_bridge_info(user_id=user_id).split(",")
+    prefix, _ = user_bridge_info(user_id=user_id)
     return prefix
 
 
 def user_bridge_account_id(user_id: str) -> str:
-    _, account_id = user_bridge_info(user_id=user_id).split(",")
+    _, account_id = user_bridge_info(user_id=user_id)
     return account_id
 
 
