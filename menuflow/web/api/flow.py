f33631edf64596cc9399ac7e5a839c9d0d091e9c
fduqueg <fduque@ikono.com.co>
2025-06-18 17:33:04 -0500

    refactor: :recycle: Refactor flow endpoints for modules

--- menuflow/web/api/flow.py
+++ menuflow/web/api/flow.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 from json import JSONDecodeError
-from typing import Dict
+from logging import Logger, getLogger
 
 from aiohttp import web
 
@@ -9,9 +9,13 @@ from ...config import Config
 from ...db.client import Client as DBClient
 from ...db.flow import Flow as DBFlow
 from ...db.flow_backup import FlowBackup
+from ...db.module import Module as DBModule
 from ...menu import MenuClient
 from ..base import get_config, routes
 from ..responses import resp
+from ..util import Util
+
+log: Logger = getLogger("menuflow.api.flow")
 
 
 # Update or create new flow
@@ -58,7 +62,7 @@ async def create_or_update_flow(request: web.Request) -> web.Response:
     """
     config: Config = get_config()
     try:
-        data: Dict = await request.json()
+        data: dict = await request.json()
     except JSONDecodeError:
         return resp.body_not_json
 
@@ -68,12 +72,47 @@ async def create_or_update_flow(request: web.Request) -> web.Response:
     if not incoming_flow:
         return resp.bad_request("Parameter flow is required")
 
+    flow_vars = incoming_flow.get("menu", {}).get("flow_variables", {})
+    nodes, positions = Util.parse_flow_to_module_fmt(incoming_flow)
+
     if flow_id:
         flow = await DBFlow.get_by_id(flow_id)
+        if not flow:
+            return resp.not_found(f"Flow with ID {flow_id} not found")
+
+        modules = await DBModule.all(int(flow_id))
+
+        if modules:
+            for module_obj in modules:
+                name = module_obj.name
+                if name not in nodes:
+                    await module_obj.delete()
+                else:
+                    new_nodes = nodes.get(name, {}).get("nodes", [])
+                    if module_obj.nodes != new_nodes:
+                        module_obj.nodes = new_nodes
+                    nodes.pop(name, None)
+
+                    new_position = positions.pop(name, {})
+                    if module_obj.position != new_position:
+                        module_obj.position = new_position
+
+                    await module_obj.update()
+
+        for name, node in nodes.items():
+            module_obj = DBModule(
+                flow_id=flow_id,
+                name=name,
+                nodes=node.get("nodes", []),
+                position=positions.get(name, {}),
+            )
+            await module_obj.insert()
+
         if flow.flow != incoming_flow:
             await flow.backup_flow(config)
 
         flow.flow = incoming_flow
+        flow.flow_vars = flow_vars
         await flow.update()
 
         if config["menuflow.load_flow_from"] == "database":
@@ -86,8 +125,18 @@ async def create_or_update_flow(request: web.Request) -> web.Response:
                 )
         message = "Flow updated successfully"
     else:
-        new_flow = DBFlow(flow=incoming_flow)
+        new_flow = DBFlow(flow=incoming_flow, flow_vars=flow_vars)
         flow_id = await new_flow.insert()
+
+        for name, node in nodes.items():
+            module_obj = DBModule(
+                flow_id=flow_id,
+                name=name,
+                nodes=node.get("nodes", []),
+                position=positions.get(name, {}),
+            )
+            await module_obj.insert()
+
         message = "Flow created successfully"
 
     return resp.ok({"detail": {"message": message, "data": {"flow_id": flow_id}}})
@@ -112,6 +161,12 @@ async def get_flow(request: web.Request) -> web.Response:
           schema:
             type: string
           description: The client MXID to get the flow.
+        - in: query
+          name: flow_format
+          schema:
+            type: boolean
+            default: false
+          description: Return the old flow format.
 
     responses:
         '200':
@@ -121,21 +176,140 @@ async def get_flow(request: web.Request) -> web.Response:
     """
     flow_id = request.query.get("id", None)
     client_mxid = request.query.get("client_mxid", None)
-    if flow_id:
-        flow = await DBFlow.get_by_id(int(flow_id))
-        if not flow:
-            return resp.not_found(f"Flow with ID {flow_id} not found")
-        data = flow.serialize()
-    elif client_mxid:
-        flow = await DBFlow.get_by_mxid(client_mxid)
-        if not flow:
-            return resp.not_found(f"Flow with mxid {client_mxid} not found")
-        data = flow.serialize()
+    flow_format = request.query.get(
+        "flow_format", False
+    )  # TODO: Remove this after a stable release of modules
+
+    if flow_id or client_mxid:
+        if flow_id:
+            flow = await DBFlow.get_by_id(int(flow_id))
+            if not flow:
+                return resp.not_found(f"Flow with ID {flow_id} not found")
+        else:
+            flow = await DBFlow.get_by_mxid(client_mxid)
+            if not flow:
+                return resp.not_found(f"Flow with mxid {client_mxid} not found")
+            flow_id = flow.id
+
+        modules = await DBModule.all(int(flow_id))
+
+        if not flow_format and modules:
+            nodes, positions = Util.parse_module_to_flow_fmt(modules)
+            data = {
+                "id": flow.id,
+                "flow": {
+                    "menu": {
+                        "flow_variables": flow.flow_vars,
+                        "nodes": nodes,
+                    },
+                    "modules": positions,
+                },
+            }
+        else:  # TODO: This is a temporary solution to return the flow when they are not yet in the module table
+            data = flow.serialize()
+            data.pop("flow_vars")
+
     else:
         flows = await DBFlow.all()
-        data = {"flows": flows}
+        if flow_format:
+            data = {"flows": flows}
+        else:
+            list_flows = []
+            for flow in flows:
+                modules = [module.serialize() for module in await DBModule.all(flow.get("id"))]
+                if modules:
+                    nodes, positions = Util.parse_module_to_flow_fmt(modules)
+                    list_flows.append(
+                        {
+                            "id": flow.get("id"),
+                            "flow": {
+                                "menu": {
+                                    "flow_variables": flow.get("flow_vars", {}),
+                                    "nodes": nodes,
+                                },
+                                "modules": positions,
+                            },
+                        }
+                    )
+                else:  # TODO: This is a temporary solution to return the flow when they are not yet in the module table
+                    flow.pop("flow_vars")
+                    list_flows.append(flow)
+
+            data = {"flows": list_flows}
 
     return resp.ok(data)
+
+
+@routes.get("/v1/flow/{flow_identifier}/nodes", allow_head=False)
+async def get_flow_nodes(request: web.Request) -> web.Response:
+    """
+    ---
+    summary: Get flow nodes by ID or client MXID
+    tags:
+        - Flow
+
+    parameters:
+        - in: path
+          name: flow_identifier
+          schema:
+            type: string
+          required: true
+          description: The flow identifier to obtain can be `id` or `mxid`.
+        - in: query
+          name: flow_format
+          schema:
+            type: boolean
+            default: false
+          description: Return the old flow format.
+        - in: query
+          name: filters_nodes
+          schema:
+            type: array
+            default: ["id", "type", "name"]
+            items:
+              type: string
+          description: List of nodes to filter.
+
+    responses:
+        '200':
+            $ref: '#/components/responses/GetFlowNodesSuccess'
+        '404':
+            $ref: '#/components/responses/GetFlowNotFound'
+    """
+    flow_identifier = request.match_info["flow_identifier"]
+
+    try:
+        flow_identifier = int(flow_identifier)
+    except Exception as e:
+        pass
+
+    flow_format = request.query.get(
+        "flow_format", False
+    )  # TODO: Remove this after a stable release of modules
+    filters_nodes = request.query.getall("filters_nodes", ["id", "type", "name"])
+
+    if isinstance(flow_identifier, int):
+        flow = await DBFlow.get_by_id(flow_identifier)
+        if not flow:
+            return resp.not_found(f"Flow with ID {flow_identifier} not found")
+    else:
+        flow = await DBFlow.get_by_mxid(flow_identifier)
+        if not flow:
+            return resp.not_found(f"Flow with mxid {flow_identifier} not found")
+
+    flow_id = flow.id
+    modules = await DBModule.all(int(flow_id))
+    list_nodes = []
+
+    if not flow_format and modules:
+        for module in modules:
+            list_nodes.extend(Util.filter_nodes_by_keys(module.nodes, filters_nodes))
+    else:  # TODO: This is a temporary solution to return the flow when they are not yet in the module table
+        data = flow.serialize()
+        nodes = data.get("flow", {}).get("menu", {}).get("nodes", [])
+        list_nodes.extend(Util.filter_nodes_by_keys(nodes, filters_nodes))
+
+    return resp.ok({"id": flow.id, "nodes": list_nodes})
 
 
 @routes.get("/v1/flow/{flow_id}/backup", allow_head=False)
--- menuflow/web/util.py
+++ menuflow/web/util.py
@@ -1,12 +1,33 @@
 import uuid
 from copy import deepcopy
 from logging import Logger, getLogger
+from textwrap import indent
 
 log: Logger = getLogger("menuflow.web.util")
 
 
 class Util:
     """Class with utility functions."""
+
+    @staticmethod
+    def docstring(doc: str):
+        """Decorator to add docstring to a function.
+
+        Parameters
+        ----------
+        doc: str
+            The docstring to add to the function.
+        Returns
+        -------
+        function
+            The function with the docstring added.
+        """
+
+        def wrapper(func):
+            func.__doc__ = doc
+            return func
+
+        return wrapper
 
     @staticmethod
     def generate_uuid() -> str:
@@ -46,3 +67,92 @@ class Util:
             update(data)
 
         return data
+
+    @staticmethod
+    def parse_module_to_flow_fmt(
+        modules: list[dict],
+    ) -> tuple[list, list]:
+        """Parse a flow object.
+
+        Returns:
+            tuple[list, list]: The parsed flow object.
+        """
+        nodes = []
+        positions = []
+        data = deepcopy(modules)
+
+        for module in data:
+            if module.get("nodes"):
+                for node in module.get("nodes"):
+                    node.setdefault(
+                        "module", module.get("name")
+                    )  # setdefault is used to avoid an exception if the module name is not in the node
+                nodes.extend(module.get("nodes"))
+            if module.get("position"):
+                module.get("position").setdefault("module", module.get("name"))
+                positions.append(module.get("position"))
+
+        return nodes, positions
+
+    @staticmethod
+    def parse_flow_to_module_fmt(
+        flow: dict,
+    ) -> tuple[dict, dict]:
+        """Parse a flow object to a list of modules.
+
+        Args:
+            flow (dict): The flow object to parse.
+
+        Returns:
+            tuple[dict, dict]: The parsed flow object.
+        """
+        positions = {}
+        flow_copy = deepcopy(flow)
+
+        for position in flow_copy.get("modules", {}):
+            name = position.pop("module")
+            positions[name] = position
+
+        nodes = {}
+
+        for node in flow_copy.get("menu", {}).get("nodes", []):
+            name = node.pop("module")
+            if name not in nodes:
+                nodes[name] = {"nodes": []}
+            nodes[name]["nodes"].append(node)
+
+        return nodes, positions
+
+    @staticmethod
+    def filter_nodes_by_keys(
+        nodes: list[dict],
+        keys_to_keep: list[str] = None,
+    ) -> list[dict]:
+        """keys_to_keep a list of nodes.
+
+        Args:
+            nodes (list[dict]): The list of nodes to keys_to_keep.
+        """
+        filtered_nodes = [
+            {key: node[key] for key in keys_to_keep if key in node} for node in nodes
+        ]
+        return [node for node in filtered_nodes if node]
+
+    @staticmethod
+    def parse_template_indent(template: str, indent_level: int = None) -> str:
+        """Get the example with the given indent level.
+
+        Parameters
+        ----------
+        template: str
+            The template to get the indent level from.
+        indent_level: int
+            The indent level to get the template with.
+
+        Returns
+        -------
+        str
+            The example with the given indent level.
+        """
+        lines = template.strip().splitlines()
+        return lines[0] + "\n" + indent("\n".join(lines[1:]), " " * (indent_level or 20))
