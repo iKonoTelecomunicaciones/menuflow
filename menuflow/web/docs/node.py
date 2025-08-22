33959aedf8348cd049435580ee14c64071415889
fduqueg <fduque@ikono.com.co>
2025-08-22 14:27:50 -0500

    feat: :sparkles: Added node list endpoint

--- menuflow/web/docs/node.py
+++ menuflow/web/docs/node.py
@@ -1,6 +1,6 @@
 from logging import Logger, getLogger
 
-log: Logger = getLogger("ivrflow.docs.node")
+log: Logger = getLogger("menuflow.docs.node")
 
 
 get_node_doc = """
@@ -36,6 +36,46 @@ get_node_doc = """
             $ref: '#/components/responses/GetNodeBadRequest'
         '404':
             $ref: '#/components/responses/GetNodeNotFound'
+        '500':
+            $ref: '#/components/responses/InternalServerError'
+"""
+
+get_node_list_doc = """
+    ---
+    summary: Get a list of nodes.
+    description: Get a list of nodes.
+
+    tags:
+        - Node
+
+    parameters:
+        - name: flow_id
+          in: path
+          required: true
+          description: The ID of the flow to get the nodes.
+          schema:
+            type: integer
+        - in: query
+          name: node_fields
+          schema:
+            type: array
+            default: ["id", "name", "type"]
+            items:
+              type: string
+          description: Fields to return the list of nodes.
+        - in: query
+          name: module_fields
+          schema:
+            type: array
+            default: ["id"]
+            items:
+              type: string
+          description: Fields to return the list of modules.
+    responses:
+        '200':
+            $ref: '#/components/responses/GetListNodesSuccess'
+        '404':
+            $ref: '#/components/responses/GetListNodesNotFound'
         '500':
             $ref: '#/components/responses/InternalServerError'
 """
