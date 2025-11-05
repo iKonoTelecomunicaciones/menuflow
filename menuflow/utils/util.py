49364fe5673012f8d7a9c9944881cd47b39c51e7
fduqueg <fduque@ikono.com.co>
2025-11-05 09:20:34 -0500

    refactor: :recycle: Changed comment in convert_to_type function

--- menuflow/utils/util.py
+++ menuflow/utils/util.py
@@ -668,8 +668,8 @@ class Util:
 
         """
 
-        # If the value is a string in double quotes, return the value as a string,
-        # since this indicates that the data type must be respected.
+        # If the value is a string in double quotes like "'Hello'" or '"World"',
+        # remove the outer quotes and return the value as a string.
         enclosers = value[0] + value[-1] if value else ""
         if enclosers == '""' or enclosers == "''":
             return value[1:-1]
